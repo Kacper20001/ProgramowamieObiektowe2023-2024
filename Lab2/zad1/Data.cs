a071710f093f3b86471c2da1fad2332b07165259
Kacper Kulig <kuligkacper20001@gmail.com>
2023-11-06 19:38:19 +0100

    Lab1 oraz Lab2

---
+++ Lab2/zad1/Data.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    internal class Data
+    {
+        public DateTime data;
+
+        public Data()
+        {
+            data = DateTime.Now;
+        
+        }
+         public void PrzesunTydzien()
+        {
+            data = data.AddDays(7);
+        }
+        
+        public void CofnijTydzien()
+        {
+            data = data.AddDays(-7);
+        }
+    }
+}
---
+++ Lab2/zad1/Liczba.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad1
+{
+    internal class Liczba
+    {
+        private int[] cyfry;
+        public Liczba(string napis)
+        {
+            Konwertuj(napis);
+        }
+        public void Konwertuj(string napis)
+        {
+            int dlugosc = napis.Length;
+            cyfry = new int[dlugosc];
+
+            for (int i = 0; i < dlugosc; i++)
+            {
+                if (char.IsDigit(napis[i]))
+                {
+                    cyfry[i] = int.Parse(napis[i].ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Konwersja niemożliwa");
+                }
+            }
+        }
+        public void Wypisz()
+        {
+            Console.WriteLine(string.Join("",cyfry));
+        }
+        public void Pomnoz(int x)
+        {
+            int reszta = 0;
+            for (int i = cyfry.Length - 1; i >= 0; i--)
+            {
+                int wynik = cyfry[i] * x + reszta;
+                cyfry[i] = wynik % 10;
+                reszta = wynik / 10;
+            }
+            if (reszta > 0 )
+            {
+                int[] tab = new int[cyfry.Length + 1];
+                cyfry.CopyTo(tab, 1);
+                tab[0] = reszta;
+                cyfry = tab;
+            }
+
+        }
+        public void Silnia(int n)
+        {
+            Konwertuj("1");
+            for (int i = 2; i <=n; i++)
+            {
+                Pomnoz(i);
+            }
+        }
+    }
+}
