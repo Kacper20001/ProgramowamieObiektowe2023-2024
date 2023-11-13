89cc043f2163bd8ac56bb193e92f933f27dee86c
Kacper Kulig <kuligkacper20001@gmail.com>
2023-11-13 23:46:24 +0100

    Dodanie Lab.2

---
+++ Lab3/zad.2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace zad._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SamochodOsobowy samochodOsobowy1 = new SamochodOsobowy();
+            samochodOsobowy1.WyswietlInformacjeSamochod();
+
+            Samochod samochod1 = new Samochod();
+            samochod1.WyswietlInformacjeSamochod();
+
+            Samochod samochod2 = new Samochod("Ford", "Mondeo", "kombi", "srebrny", 2018, 210000);
+            samochod2.WyswietlInformacjeSamochod();
+        }
+    }
+}
